a83d5420722293e302b2e613e5745b5c92b02cc9
Roger Rangel <roger.rangel.ee@gmail.com>
2023-03-21 17:41:37 +0100

    Add packages

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Therapist</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>AI Therapist</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Are you feeling sad today? Talk to one of our experts</h2>
           </div>
         </div>
       </div>
